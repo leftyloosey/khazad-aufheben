8623689468f8fb3b6b3d516ca85757e002fcb876
davidhardin <thisgoestodavid@gmail.com>
2023-06-03 13:09:35 -0600

    routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import products from '../products.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import connectDB from './config/db.js'
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
--- products.js
+++ products.js
@@ -1,6 +1,5 @@
 const products = [
     {
-        _id: 1,
         name: 'Airpods Wireless Bluetooth Headphones',
         image: '/images/airpods.jpg',
         description:
@@ -13,8 +12,6 @@ const products = [
         numReviews: 12,
     },
     {
-        _id: 2,
-
         name: 'iPhone 13 Pro 256GB Memory',
         image: '/images/phone.jpg',
         description:
@@ -27,8 +24,6 @@ const products = [
         numReviews: 8,
     },
     {
-        _id: 3,
-
         name: 'Cannon EOS 80D DSLR Camera',
         image: '/images/camera.jpg',
         description:
@@ -41,8 +36,6 @@ const products = [
         numReviews: 12,
     },
     {
-        _id: 4,
-
         name: 'Sony Playstation 5',
         image: '/images/playstation.jpg',
         description:
@@ -55,8 +48,6 @@ const products = [
         numReviews: 12,
     },
     {
-        _id: 5,
-
         name: 'Logitech G-Series Gaming Mouse',
         image: '/images/mouse.jpg',
         description:
@@ -69,8 +60,6 @@ const products = [
         numReviews: 10,
     },
     {
-        _id: 6,
-
         name: 'Amazon Echo Dot 3rd Generation',
         image: '/images/alexa.jpg',
         description:
