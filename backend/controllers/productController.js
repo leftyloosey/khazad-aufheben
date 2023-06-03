e47d219e43a61bd0ff8eb5c209c3bd11131fa8e2
davidhardin <thisgoestodavid@gmail.com>
2023-06-03 14:29:21 -0600

    more routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,28 @@
+import asyncHandler from '../middleware/asyncHandler.js'
+import Product from '../models/productModel.js'
+
+// @desc fetch all products
+// GET /api/products
+// @access Public
+
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+// @desc Fetch one product
+// GET /api/products
+// @access public
+
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        return res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Resource not found')
+    }
+})
+
+export { getProductById, getProducts }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express'
-// import products from '../products.js'
 import productRoutes from './routes/productRoutes.js'
 import dotenv from 'dotenv'
 dotenv.config()
-
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 const port = process.env.PORT || 5000
 
 connectDB()
@@ -16,5 +16,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
