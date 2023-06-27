0439617c037628074d37f62049fa243c406186d0
davidhardin <thisgoestodavid@gmail.com>
2023-06-27 14:57:30 -0600

    more order stuff

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,8 +1,13 @@
 import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { toast } from 'react-toastify'
 import CheckoutSteps from '../components/CheckoutSteps'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { useCreateOrderMutation } from '../slices/ordersApiSlice'
+import { clearCartItems } from '../slices/cartSlice'
 
 const PlaceOrderScreen = () => {
     const navigate = useNavigate()
@@ -20,7 +25,18 @@ const PlaceOrderScreen = () => {
     <>
       <CheckoutSteps step1 step2 step3 step4 />
       <Row>
-        <Col md={8}>Column</Col>
+        <Col md={8}>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h2>Shipping</h2>
+                    <p>
+                        <strong>Address:</strong>
+                        {cart.shippingAddress.address}, {cart.shippingAddress.city},
+                        {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
+                    </p>
+                </ListGroup.Item>
+            </ListGroup>
+        </Col>
         <Col md={4}>Column</Col>
       </Row>
     </>
