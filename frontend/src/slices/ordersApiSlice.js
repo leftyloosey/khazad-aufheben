b6ce94b641ce5f7d220e93b65f347217deecbcec
davidhardin <thisgoestodavid@gmail.com>
2023-07-12 17:08:50 -0600

    order details

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -30,6 +30,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5,
         }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/mine`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
     }),
 })
 
@@ -38,6 +44,7 @@ export const {
     useGetOrderDetailsQuery,
     usePayOrderMutation,
     useGetPaypalClientIdQuery,
+    useGetMyOrdersQuery,
 } = ordersApiSlice
 // import { apiSlice } from './apiSlice'
 // import { ORDERS_URL, PAYPAL_URL } from '../constants'
