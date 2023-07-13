a6dd32b2de12df8457785b41328e0ea28dc7ee3a
davidhardin <thisgoestodavid@gmail.com>
2023-07-13 16:57:13 -0600

    products yet again

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,5 +1,5 @@
 import { LinkContainer } from 'react-router-bootstrap'
-import { Table, Button } from 'react-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
 import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
@@ -7,12 +7,19 @@ import { useGetProductsQuery } from '../../slices/productsApiSlice'
 
 const ProductListScreen = () => {
     const { data: products, isLoading, error } = useGetProductsQuery()
-    console.log(products)
-  return (
-    <div>
-      ProductListScreen
-    </div>
-  )
+  return <>
+    <Row className='align-items-center'>
+        <Col>
+            <h1>Products</h1>
+        </Col>
+        <Col className='text-end'>
+            <Button className='btn-sm m-3'>
+                <FaEdit /> Create Product
+            </Button>
+        </Col>
+    </Row>
+    
+    </>
 }
 
 export default ProductListScreen
