178ed612254906ed8037d25b4ec99c9a68b0fb38
davidhardin <thisgoestodavid@gmail.com>
2023-07-20 15:08:15 -0600

    productlist loader

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,7 @@
 import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button, Row, Col } from 'react-bootstrap'
 import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa'
+import { toast } from 'react-toastify'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
 import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice'
@@ -10,18 +11,30 @@ const ProductListScreen = () => {
 
     const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation
 
-    const deleteHandler = () => {}    
+    const deleteHandler = (id) => {
+        console.log('delete', id)
+    }    
+    const createProductHandler = async () => {
+        if (window.confirm('Are you sure?')) {
+            try {
+                await createProduct()
+            } catch (err) {
+                toast.error(err?.data?.emssage || err.error)
+            }
+        }
+    }    
   return <>
     <Row className='align-items-center'>
         <Col>
             <h1>Products</h1>
         </Col>
         <Col className='text-end'>
-            <Button className='btn-sm m-3'>
+            <Button className='btn-sm m-3' onClick={createProductHandler}>
                 <FaEdit /> Create Product
             </Button>
         </Col>
     </Row>
+    {}
     {isLoading ? <Loader /> : error ? <Message variant='danger'>
         {error}</Message> : (
             <>
