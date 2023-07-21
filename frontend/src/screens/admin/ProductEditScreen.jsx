26344778f075945f336576de9ca536e835593531
davidhardin <thisgoestodavid@gmail.com>
2023-07-21 11:33:51 -0600

    paco de lucia wants mongo to react

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -36,6 +36,27 @@ const ProductEditScreen = () => {
     }
   }, [product])
 
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      await updateProduct({
+        productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        description,
+        countInStock,
+      });
+      toast.success('Product updated');
+      refetch();
+      navigate('/admin/productlist');
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  }
+
   return (
     <>
      <Link to='/admin/productlist' className='btn btn-light my-3'>
@@ -48,17 +69,17 @@ const ProductEditScreen = () => {
         {isLoading ? <Loader /> : error ? <Message variant='danger'>
           {error}
         </Message> : (
-          <Form>
-            <Form.Group controlId='name'>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name' className='my-2'>
               <Form.Label>Name</Form.Label>
               <Form.Control 
-              type='name'
+              type='text'
               placeholder='Enter name'
               value={name}
               onChange={(e) => setName(e.target.value)}> 
               </Form.Control>
               </Form.Group>
-            <Form.Group controlId='price'>
+            <Form.Group controlId='price' className='my-2'>
               <Form.Label>Price</Form.Label>
               <Form.Control 
               type='number'
@@ -67,6 +88,46 @@ const ProductEditScreen = () => {
               onChange={(e) => setPrice(e.target.value)}> 
               </Form.Control>
             </Form.Group>
+            {/* image input placeholder */}
+            <Form.Group controlId='brand' className='my-2'>
+              <Form.Label>Brand</Form.Label>
+              <Form.Control 
+              type='text'
+              placeholder='Enter brand'
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}> 
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId='countInStock' className='my-2'>
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control 
+              type='number'
+              placeholder='Enter Count In Stock'
+              value={countInStock}
+              onChange={(e) => setCountInStock(e.target.value)}> 
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId='category' className='my-2'>
+              <Form.Label>Category</Form.Label>
+              <Form.Control 
+              type='text'
+              placeholder='Enter Count In Stock'
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}> 
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId='description' className='my-2'>
+              <Form.Label>Description</Form.Label>
+              <Form.Control 
+              type='text'
+              placeholder='Description'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}> 
+              </Form.Control>
+            </Form.Group>
+            <Button type='submit' variant='primary' className='my-2'>
+              Update
+            </Button>
           </Form>
         )}  
       </FormContainer> 
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -9,7 +9,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 // url: '',
             }),
             keepUnusedDataFor: 5,
-            providesTags: ['Products'],
+            providesTags: ['Product'],
         }),
         getProductDetails: builder.query({
             query: (productId) => ({
@@ -26,7 +26,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         }),
         updateProduct: builder.mutation({
             query: (data) => ({
-                url: `${PRODUCTS_URL}/${data._id}`,
+                url: `${PRODUCTS_URL}/${data.productId}`,
                 method: 'PUT',
                 body: data,
             }),
