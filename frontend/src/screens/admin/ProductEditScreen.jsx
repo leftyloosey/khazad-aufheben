8ab0fcb32338308bf7130a9d069293f1119be358
davidhardin <thisgoestodavid@gmail.com>
2023-07-21 11:09:53 -0600

    edit screen

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -19,11 +19,58 @@ const ProductEditScreen = () => {
   const [description, setDescription] = useState('')
 
   const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId)
+
+  const [updateProduct, { isLoading: loadingUpdate }] = useUpdateProductMutation()
   
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (product) {
+      setName(product.name)
+      setPrice(product.price)
+      setImage(product.image)
+      setBrand(product.brand)
+      setCategory(product.category)
+      setCountInStock(product.countInStock)
+      setDescription(product.description)
+    }
+  }, [product])
+
   return (
-    <div>
-      Product Edit Screen
-    </div>
+    <>
+     <Link to='/admin/productlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+
+        {isLoading ? <Loader /> : error ? <Message variant='danger'>
+          {error}
+        </Message> : (
+          <Form>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control 
+              type='name'
+              placeholder='Enter name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}> 
+              </Form.Control>
+              </Form.Group>
+            <Form.Group controlId='price'>
+              <Form.Label>Price</Form.Label>
+              <Form.Control 
+              type='number'
+              placeholder='Enter price'
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}> 
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        )}  
+      </FormContainer> 
+    </>
   )
 }
 
