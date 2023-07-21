0b534d2c49bd9e8d305c311b0f4fd5fcdc6db036
davidhardin <thisgoestodavid@gmail.com>
2023-07-21 12:19:42 -0600

    multer stuff

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import path from 'path'
 import express from 'express'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 dotenv.config()
@@ -16,18 +18,10 @@ connectDB()
 const app = express()
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 // app.use(cors)
-// app.use(cors({ origin: '*', credentials: true }))
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// app.use(
-//     cors({
-//         credentials: true,
-//         origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
-//         optionsSuccessStatus: 200,
-//     })
-// )
 
 app.get('/', (req, res) => {
     res.send('API running...')
@@ -36,10 +30,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
+
+const __dirname = path.resolve() // set __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
