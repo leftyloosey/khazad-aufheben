848d0705e3bdbcaf1772ce23edcea7c42015646b
davidhardin <thisgoestodavid@gmail.com>
2023-07-28 14:39:53 -0600

    search box

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,29 +1,77 @@
-import { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import { useParams, useNavigate } from 'react-router-dom'
+// import { useState } from 'react'
+// import { Form, Button } from 'react-bootstrap'
+// import { useParams, useNavigate } from 'react-router-dom'
+
+// const SearchBox = () => {
+//   const navigate = useNavigate()
+//   const { keyword: urlKeyword } = useParams()
+//   const [keyword, setKeyword] = useState(urlKeyword || '')
+
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+//     if (keyword) {
+//       navigate(`/search/${keyword.trim()}`);
+//       setKeyword('');
+//     } else {
+//       navigate('/');
+//     }
+//   }
+
+//   return (
+//       <Form onSubmit={submitHandler} className='d-flex'>
+//         <Form.Control
+//             type='text'
+//             name='q'
+//             onChange={(e) => setKeyword(e.target.value)}
+//             value={keyword}
+//             placeholder='Search Products...'
+//             className='mr-sm-2 ml-sm-5'
+//             >
+//                 <Button type='submit' variant='outline-success' className='p-2 mx-2'>
+//                     Search
+//                 </Button>
+//             </Form.Control>
+
+//       </Form>
+//   )
+// }
+
+// export default SearchBox
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBox = () => {
-  const navigate = useNavigate()
-  const { keyword: urlKeyword } = useParams()
-  const [keyword, setKeyword] = useState(urlKeyword || '')
+  const navigate = useNavigate();
+  const { keyword: urlKeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlKeyword);
 
-  const submitHandler = () => {}
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword) {
+      navigate(`/search/${keyword.trim()}`);
+      setKeyword('');
+    } else {
+      navigate('/');
+    }
+  };
 
   return (
-      <Form onSubmit={submitHandler} className='d-flex'>
-        <Form.Control
-            type='text'
-            name='q'
-            onChange={(e) => setKeyword(e.target.value)}
-            value={keyword}
-            placeholder='Search Products...'
-            className='mr-sm-2 ml-sm-5'
-            >
-                <Button type='submit' variant='outline-success' className='p-2'></Button>
-            </Form.Control>
+    <Form onSubmit={submitHandler} className='d-flex'>
+      <Form.Control
+        type='text'
+        name='q'
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        placeholder='Search Products...'
+        className='mr-sm-2 ml-sm-5'
+      ></Form.Control>
+      <Button type='submit' variant='outline-success' className='p-2 mx-2'>
+        Search
+      </Button>
+    </Form>
+  );
+};
 
-      </Form>
-  )
-}
-
-export default SearchBox
+export default SearchBox;
