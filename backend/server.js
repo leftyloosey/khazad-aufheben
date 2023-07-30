0caac8662e71b1b11d858a0a047f756f1a3e3791
davidhardin <thisgoestodavid@gmail.com>
2023-07-30 14:53:12 -0600

    heroku woes

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-dotenv.config()
+// dotenv.config()
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 // import cors from 'cors'
