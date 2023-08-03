f1823384aa6b7dc30a117a4ec66b23c2a9c80e97
davidhardin <thisgoestodavid@gmail.com>
2023-08-03 17:48:32 -0600

    heroku woes

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/user', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
